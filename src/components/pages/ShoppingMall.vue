79cad0e0d2f3fb2425da86694134c248c11d02a6
XuBill <123495351@qq.com>
2018-05-19 07:19:58 +0800

    task#3 按文字版模仿完成，今天看下视频版，复习下

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -26,7 +26,7 @@
         <div class="type-bar">
             <div v-for="(cate,index) in category" :key="index">
                  <img v-lazy="cate.image" width="90%" />
-                 <span>{{cate.mallCategoryName}}</span> 
+                 <span>{{cate.mallCategoryName}}</span>
             </div>
         </div>
 
@@ -51,10 +51,31 @@
                 <div class="recommend-item">
                     <img :src="item.image" width="80%">
                     <div>{{item.goodsName}}</div>
-                    <div>￥{{item.price}}(￥{{item.mallPrice}})</div>
+                    <div>￥{{item.price | moneyFilter}}(￥{{item.mallPrice | moneyFilter}})</div>
                 </div>
             </swiper-slide>
         </swiper>
+
+        <floorComponent :floorData="floor1" :floorTitle="floorName.floor1"></floorComponent>
+        <floorComponent :floorData="floor2" :floorTitle="floorName.floor2"></floorComponent>
+        <floorComponent :floorData="floor3" :floorTitle="floorName.floor3"></floorComponent>
+
+        <!-- hot area -->
+        <div class="hot-area">
+          <div class="hot-title">
+            <div class="hot-goods">
+              <van-list>
+                <van-row gutter="20">
+                  <van-col span="12" v-for="(item,index) in hotGoods" :key="index">
+                    <goods-info :goodsImage="item.image" :goodsName="item.name" :goodsPrice="item.price"></goods-info>
+                  </van-col>
+                </van-row>
+              </van-list>
+            </div>
+          </div>
+        </div>
+
+        </div>
     </div>
 </template>
 
@@ -62,7 +83,10 @@
     import axios from 'axios'
     import 'swiper/dist/css/swiper.css'
     import { swiper, swiperSlide } from 'vue-awesome-swiper'
- 
+    import floorComponent from '../component/floorComponent'
+    import {toMoney} from '@/filter/moneyFilter.js'
+    import goodsInfo from '../component/goodsInfoComponent'
+
     export default {
         data() {
             return {
@@ -71,11 +95,18 @@
                 category:[],
                 adBanner:'',
                 recommendGoods:[],
+                floor1:[],
+                floor2:[],
+                floor3:[],
+                floorName:{},
+                hotGoods:[] //热卖商品
             }
         },
         components: {
             swiper,
-            swiperSlide
+            swiperSlide,
+            floorComponent,
+            goodsInfo
         },
         created(){
             console.log("------------------------------");
@@ -90,13 +121,23 @@
                     this.bannerPicArray = response.data.data.slides;
                     this.category=response.data.data.category;
                     this.adBanner = response.data.data.advertesPicture.PICTURE_ADDRESS; //获得广告图片
+                    this.floor1 = response.data.data.floor1              //楼层1数据
+                    this.floor2 = response.data.data.floor2              //楼层2数据
+                    this.floor3 = response.data.data.floor3              //楼层3数据
+                    this.floorName = response.data.data.floorName
+                    this.hotGoods = response.data.data.hotGoods          //楼层名称
                 }
             })
             .catch(error=>{
                 console.log(error);
             })
-        }
-    }    
+        },
+        filters: {
+          moneyFilter(money){
+            return toMoney(money);
+          }
+        },
+    }
 </script>
 
 <style scoped>
@@ -104,7 +145,7 @@
       height: 2.2rem;
       background-color: #e5017d;
       line-height:2.2rem;
- 
+
   }
   .search-input{
       width:100%;
@@ -156,5 +197,43 @@
       border-right: 1px solid #eee;
       font-size: 12px;
       text-align: center;
+  }
+  .floor-anomaly{
+    display: flex;
+    flex-direction: row;
+    background-color: #fff;
+    border-bottom: 1px solid #ddd;
+  }
+  .floor-anomaly div{
+    width: 10rem;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box
+  }
+  .floor-one{
+    border-right: 1px solid #ddd
+  }
+  .floor-two{
+    border-right: 1px solid #ddd
+  }
+  .floor-rule{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    background-color: #fff;
+  }
+  .floor-rule div{
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    width: 10rem;
+    border-bottom: 1px solid #ddd;
+  }
+  .floor-rule div:nth-child(odd){
+    border-right: 1px solid #ddd;
+  }
+  .hot-area{
+    text-align: center;
+    font-size: 14px;
+    height: 1.8rem;
+    line-height: 1.8rem;
   }
 </style>
