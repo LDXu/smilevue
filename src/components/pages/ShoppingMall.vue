2d3abff587b21d307ec6c1d24be5ae7ae05d1804
XuBill <123495351@qq.com>
2018-09-25 19:36:04 +0800

    任务4

---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,34 @@
+<!--  -->
+<template>
+  <div>
+    <van-row class="test-row">
+      <van-col span="8">span</van-col>
+      <van-col span="8">span</van-col>
+      <van-col span="8">span</van-col>
+    </van-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'shoppingMall'
+    };
+  },
+
+  // components: {},
+
+  // computed: {},
+
+  // mounted: {},
+
+  // methods: {}
+}
+
+</script>
+<style scoped>
+  .test-row{
+    text-align: center;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import router from './router'
 // import Vant from 'vant'
 // import 'vant/lib/vant-css/index.css'
 // Vue.use(Vant)
-import { Button } from "vant";
+import { Button, Row, Col } from "vant";
 
-Vue.use(Button)
+Vue.use(Button).use(Row).use(Col)
 
 Vue.config.productionTip = false
 
