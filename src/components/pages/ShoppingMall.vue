99becb0083f5d5220b2bdddfc1a4f6d5b5e08ada
XuBill <123495351@qq.com>
2018-09-25 20:20:16 +0800

    课时 5 : 首页搜索布局

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -1,11 +1,21 @@
 <!--  -->
 <template>
   <div>
-    <van-row class="test-row">
-      <van-col span="8">span</van-col>
-      <van-col span="8">span</van-col>
-      <van-col span="8">span</van-col>
+    <div class="search-bar">
+      <van-row>
+      <van-col span="3">
+        <img :src="locationIcon" width="70%" class="location-icon">
+      </van-col>
+      <van-col span="16">
+        <input type="text" class="search-input">
+      </van-col>
+      <van-col span="5">
+        <van-button size="mini">
+          查找
+        </van-button>
+      </van-col>
     </van-row>
+    </div>
   </div>
 </template>
 
@@ -13,7 +23,8 @@
 export default {
   data () {
     return {
-      msg: 'shoppingMall'
+      msg: 'shoppingMall',
+      locationIcon:require('../../assets/images/location.png')
     };
   },
 
@@ -28,7 +39,23 @@ export default {
 
 </script>
 <style scoped>
-  .test-row{
-    text-align: center;
+  .search-bar{
+    height: 2.2rem;
+    background-color: #e5017d;
+    line-height: 2.2rem;
+  }
+  .search-input{
+    width: 100%;
+    height: 1.3rem;
+    border-top: 0px;
+    border-left: 0rem;
+    border-right: 0rem;
+    border-bottom: 1px solid #fff;
+    background-color: #e5017d;
+    color:#fff
+  }
+  .location-icon{
+    padding-top: .2rem;
+    padding-left: .3rem;
   }
 </style>
